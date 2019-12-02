8d42406752e48654678ff45dc982d84e6cd04895
ucgeek <39198233+ucgeek@users.noreply.github.com>
2019-12-03 07:58:24 +1300

    Create Get-TeamsAssignedNumbers.ps1

---
+++ Get-TeamsAssignedNumbers.ps1
@@ -0,0 +1,127 @@
+#region INFO
+<# 
+.SYNOPSIS
+ 
+    Get-TeamsAssignedNumbers.ps1 collects assigned phone numbers from all Microsoft Teams.
+ 
+.DESCRIPTION
+    Author: Andrew Morpeth
+    Contact: https://ucgeek.co/
+    
+    This script queries Microsoft Teams for assigned numbers and displays in a formatted table with the option to export to CSV. 
+    During processing LineURI's are run against a regex pattern to extract the DDI/DID and the extension to a separate column.
+    
+    This script collects Microsoft Teams objects including:
+    Users, Meeting Rooms, Online Application Instances (Resource Accounts)
+
+    This script does not collect objects from on-premises environments even if in hybrid, instead use this script - https://gallery.technet.microsoft.com/office/Lync-Get-All-Assigned-8c1328a0
+    
+    This script is provided as-is, no warrenty is provided or implied.The author is NOT responsible for any damages or data loss that may occur
+    through the use of this script.  Always test before using in a production environment. This script is free to use for both personal and 
+    business use, however, it may not be sold or included as part of a package that is for sale. A Service Provider may include this script 
+    as part of their service offering/best practices provided they only charge for their time to implement and support.
+
+.RUN INSTRUCTIONS 
+    If you haven’t already, you will need to install the Skype for Business Online PowerShell module which can be downloaded here - https://www.microsoft.com/en-us/download/details.aspx?id=39366
+    Update settings "Settings" at the top of the script
+    Run: .\Get-TeamsAssignedNumbers.ps1
+
+.NOTES
+    v1.0 - Initial release       
+#>
+#endregion INFO
+
+#Connect to Skyper for Business Online
+if (!$sfboSession.State -eq 'Opened')
+{
+    Import-Module SkypeOnlineConnector
+    $sfboSession = New-CsOnlineSession
+    Import-PSSession $sfboSession -AllowClobber
+}
+
+#Settings ##############################
+#. "_Settings.ps1" | Out-Null
+$FileName = "TeamsAssignedNumbers_" + (Get-Date -Format s).replace(":","-") +".csv"
+$FilePath = "D:\Temp\$FileName"
+$OutputType = "CSV" #OPTIONS: CSV - Outputs CSV to specified FilePath, CONSOLE - Outputs to console
+
+##############################
+
+$Regex1 = '^(?:tel:)?(?:\+)?(\d+)(?:;ext=(\d+))?(?:;([\w-]+))?$'
+$Array1 = @()
+#Get Users with LineURI
+$UsersLineURI = Get-CsOnlineUser -Filter {LineURI -ne $Null}
+if($UsersLineURI -ne $null)
+{
+    foreach($item in $UsersLineURI)
+    {                  
+        $Matches = @()
+        $Item.LineURI -match $Regex1 | out-null
+            
+        $myObject1 = New-Object System.Object
+        $myObject1 | Add-Member -type NoteProperty -name "LineURI" -Value $Item.LineURI
+        $myObject1 | Add-Member -type NoteProperty -name "DDI" -Value $Matches[1]
+        $myObject1 | Add-Member -type NoteProperty -name "Ext" -Value $Matches[2]
+        $myObject1 | Add-Member -type NoteProperty -name "DisplayName" -Value $Item.DisplayName
+        $myObject1 | Add-Member -type NoteProperty -name "FirstName" -Value $Item.FirstName
+        $myObject1 | Add-Member -type NoteProperty -name "LastName" -Value $Item.LastName
+        $myObject1 | Add-Member -type NoteProperty -name "Type" -Value "User"
+        $Array1 += $myObject1          
+    }
+}
+
+#Get meeting room numbers
+$MeetingRoomLineURI = Get-CsMeetingRoom -Filter {LineURI -ne $Null}
+if($MeetingRoomLineURI -ne $null)
+{
+	Write-Verbose "Processing Meeting Room Numbers"
+    foreach($Item in $MeetingRoomLineURI)
+    {                 
+        $Matches = @()
+        $Item.LineURI -match $Regex1 | out-null
+            
+        $myObject1 = New-Object System.Object
+        $myObject1 | Add-Member -type NoteProperty -name "LineURI" -Value $Item.LineURI
+        $myObject1 | Add-Member -type NoteProperty -name "DDI" -Value $Matches[1]
+        $myObject1 | Add-Member -type NoteProperty -name "Ext" -Value $Matches[2]
+        $myObject1 | Add-Member -type NoteProperty -name "DisplayName" -Value $Item.DisplayName
+        $myObject1 | Add-Member -type NoteProperty -name "Type" -Value "MeetingRoom"
+        $Array1 += $myObject1         
+    }
+}
+
+#Get online resource accounts
+$OnlineApplicationInstanceLineURI = Get-CsOnlineApplicationInstance | where {$_.PhoneNumber -ne $Null}
+if($OnlineApplicationInstanceLineURI -ne $null)
+{
+	Write-Verbose "Processing Online Application Instances (Resource Accounts) Numbers"
+    foreach($Item in $OnlineApplicationInstanceLineURI)
+    {                 
+        $Matches = @()
+        $Item.PhoneNumber -match $Regex1 | out-null
+            
+        $myObject1 = New-Object System.Object
+        $myObject1 | Add-Member -type NoteProperty -name "LineURI" -Value $Item.PhoneNumber
+        $myObject1 | Add-Member -type NoteProperty -name "DDI" -Value $Matches[1]
+        $myObject1 | Add-Member -type NoteProperty -name "Ext" -Value $Matches[2]
+        $myObject1 | Add-Member -type NoteProperty -name "DisplayName" -Value $Item.DisplayName
+        $myObject1 | Add-Member -type NoteProperty -name "Type" -Value $(if ($item.ApplicationId -eq "ce933385-9390-45d1-9512-c8d228074e07") {"Auto Attendant Resource Account"} elseif ($item.ApplicationId -eq "11cd3e2e-fccb-42ad-ad00-878b93575e07") {"Call Queue Resource Account"} else {"Unknown Resource Account"})
+        $Array1 += $myObject1         
+    }
+}
+
+if($OutputType -eq "CSV")
+{
+    $Array1 | export-csv $FilePath -NoTypeInformation
+    Write-Host "ALL DONE!! Your file has been saved to $FilePath. Press any key to quit"
+}
+elseif($OutputType -eq "CONSOLE")
+{
+    $Array1 | FT -AutoSize -Property LineURI,DDI,Ext,DisplayName,Type
+    Write-Host "ALL DONE!! Press any key to quit"
+}
+else
+{
+    $Array1 | FT -AutoSize -Property LineURI,DDI,Ext,DisplayName,Type
+    Write-Host "WARNING: Valid output type not set, defaulted to console. Press any key to quit"
+}
